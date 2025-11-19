9f2dce7bce02f4e72d1a07d39bdc13246b5af99f
Nazmul Hasan <nazmulhasannabil4@gmail.com>
2025-11-19 10:36:29 +0600

    firebase login Fixed

--- src/contexts/authContext/AuthProvider.jsx
+++ src/contexts/authContext/AuthProvider.jsx
@@ -53,9 +53,9 @@ const AuthProvider = ({ children }) => {
     }
 
     return (
-        <AuthContext value={authInfo}>
+        <AuthContext.Provider value={authInfo}>
             {children}
-        </AuthContext>
+        </AuthContext.Provider>
     )
 }
 
